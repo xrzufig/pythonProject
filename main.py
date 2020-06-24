4abb8236c4d3c061a270475d963e43f5d1c55cce
xrzufig <marcel.dziewior999@gmail.com>
2020-06-24 20:08:15 +0200

    newCommit

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def fib(n):
-    if n == 0:
-        return 1
-    elif n == 1:
-        return 1
-    else:
-        return (fib(n - 1)) + (fib(n - 2))
-print(fib(6))
+name = input('enter your name: ')
+
+print('hello, ', name)
