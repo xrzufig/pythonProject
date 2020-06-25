5952da5abba2235645d2be96b544170be6de7bee
xrzufig <marcel.dziewior999@gmail.com>
2020-06-25 14:23:53 +0200

    newestCommit

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ from tkinter import *
 #appending window
 #root = Tk()
 
+def printTask():
+    f = open('toDO.txt', 'r')
+    print(open('toDO.txt', 'r').read())
+    f.close()
+
+def decisionToDelete():
+    if choice == 'yes':
+        file = open('toDO.txt', 'r')
+        lines = file.readlines()
+        file.close()
+        which = int(input('Which task do you want to delete?: '))
+        del lines[which - 1]
+        new_file = open('toDO.txt', 'w+')
+        for line in lines:
+            new_file.write(line)
+    else:
+        print('ok, goodbye')
+        
 #entering task
 task = input('enter task: ')
 
@@ -18,23 +36,13 @@ choice = str(input('Do you want to delete task?: '))
 #making choice upper to be easier for input
 choice.upper
 #print current file
-print(open('toDO.txt', 'r').read())
+printTask()
 file.close()
 
 #choice of deleting file
-if choice == 'yes':
-    file = open('toDO.txt', 'r')
-    lines = file.readlines()
-    file.close()
-    which = int(input('Which task do you want to delete?: '))
-    del lines[which - 1]
-    new_file = open('toDO.txt', 'w+')
-    for line in lines:
-        new_file.write(line)
-else:
-    print('ok, goodbye')
+decisionToDelete()
 
-print(open('toDO.txt', 'r').read())
+printTask()
 print('goodbye!')
 
 #root.mainloop()
